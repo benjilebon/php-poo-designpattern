295cf4c7b4ac296bd3c8a0f318a6b60f47e96193
benjilebon <benji.lebon467@gmail.com>
2019-12-05 21:44:39 +0100

    [CONSOLE] Show Usage

--- Console/Handle.php
+++ Console/Handle.php
@@ -4,24 +4,34 @@ require_once 'Colors.php';
 
 class Console {
     private $primary, $secondary;
+    private $space = '  ';
+    private $c;
 
     function __construct() {
         $this->primary = "purple";
         $this->secondary = "blue";
+        $this->c = new Colors();
     }
 
     public function boot() {
-        $this->write("\n");
 
         if (!isset($argv[1])) {
-            $c = new Colors();
-            $this->write('Spacewich "Framework" '.$c->getColoredString("v0.1", $this->primary, null)."\n");
-            $this->write('By '.$c->getColoredString('Léonard Martin', $this->primary).' & '.$c->getColoredString('Benjamin Lebon', $this->primary));
+            $this->write('Spacewich "Framework" '.$this->c->getColoredString("v0.1", $this->primary, null)."\n");
+            $this->write('By '.$this->c->getColoredString('Léonard Martin', $this->primary).' & '.$this->c->getColoredString('Benjamin Lebon', $this->primary));
+            $this->showHelp();
+        } else {
+            $this->handleCommand($argv[1]);
         }
     }
 
     private function write(String $string) {
         echo $string;
+    }
+
+    private function showHelp() {
+        $this->write("\n\n");
+        $this->write($this->c->getColoredString('Usage:', $this->secondary)."\n");
+        $this->write($this->space.'php spacewich [command]');
     }
 }
 

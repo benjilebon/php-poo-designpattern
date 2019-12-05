333358b5b307e0c3c16d6050b536aa48ea407202
benjilebon <benji.lebon467@gmail.com>
2019-12-05 22:05:38 +0100

    [CONSOLE] Created Abstract Command

---
+++ Console/Command.php
@@ -0,0 +1,15 @@
+<?php
+
+abstract class Command {
+    public function showName() {
+        return $this->name;
+    }
+
+    public function showDescription() {
+        return $this->description;
+    }
+
+    abstract public function handle();
+}
+
+?>
